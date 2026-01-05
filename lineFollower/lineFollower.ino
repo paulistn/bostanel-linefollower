016194a76153eaa014c4652f3f5e5052372c8dcb
paulistn <paulioanstan@gmail.com>
2026-01-05 19:08:20 +0200

    Updated code

--- lineFollower/lineFollower.ino
+++ lineFollower/lineFollower.ino
@@ -70,6 +70,8 @@ void loop() {
   delay(10);
 }
 
+
+//Find the distance from obstacle
 long ultrasonicRead(){
   digitalWrite(trigger, LOW);
   delayMicroseconds(2); //Just make sure the pin is on LOW first
@@ -82,11 +84,68 @@ long ultrasonicRead(){
   return distance;
 }
 
+//Decides which way to continue based on the larger distance between both sides 
+void compareDistance(){
+  (if distanceLeft > distanceRight){
+    turnLeft();
+    delay(500);
+    forword(600);
+    delay(600);
+    turnRight();
+    delay(500);
+    forward();
+    delay(600);
+    turnRight();
+    delay(400);
+  }
+  else{
+    turnRight();
+    delay(500);
+    forword(600);
+    delay(600);
+    turnLeft();
+    delay(500);
+    forward();
+    delay(600);
+    turnLeft();
+    delay(400);
+  }
+}
+
+//Move the servomotor with the ultrasound sensor and xompare the distances on left and right
+void checkSide(){
+  stop();
+  delay(100);
+  for (int angle = 90; angle <= 180; angle += 1)  {
+    servo.write(angle);
+  }
+  delay(300);
+  distanceRight = ultrasonicRead();
+  Serial.print("Distance Right: ");
+  Serial.print(distanceRight);
+  delay(100);
+  for (int angle = 180; angle >= 0; angle -= 1)  {
+    servo.write(angle);
+    delay(15);  
+  }
+  delay(500);
+  distanceLeft = ultrasonicRead();
+  Serial.print("Distance Left: ");
+  Serial.print(distanceLeft);
+  delay(100);
+  for (int angle = 0; angle <= 90; angle += 1)  {
+    servo.write(angle); 
+    delay(15); 
+  }
+  servo.write(90);
+  delay(300);
+  compareDistance();
+}
+
 
 //Motor movement
-
 void forward(){
-  //goes forward
+  //goes forward - both sides go forward
   digitalWrite(in1, LOW); //Right backward LOW
   digitalWrite(in2, HIGH); //Right forward HIGH 
   digitalWrite(in3, HIGH); //Left forward HIGH
@@ -94,15 +153,36 @@ void forward(){
 }
 
 void backward(){
-  //goes backward
-  digitalWrite(in1, HIGH); 
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
+  //goes backward - both sides go backwards
+  digitalWrite(in1, HIGH); //Right backward HIGH
+  digitalWrite(in2, LOW); //Right forward LOW 
+  digitalWrite(in3, LOW); //Left forward LOW
+  digitalWrite(in4, HIGH); //Left backward HIGH
 }
 
-void turnLeft()
+void turnLeft(){
+  //left side forward, right side backward
+  digitalWrite(in1, HIGH); //Right backward HIGH
+  digitalWrite(in2, LOW); //Right forward LOW 
+  digitalWrite(in3, HIGH); //Left forward HIGH
+  digitalWrite(in4, LOW); //Left backward LOW
+}
 
+void turnRight(){
+  //left side backward, right side forward
+  digitalWrite(in1, LOW); //Right backward LOW
+  digitalWrite(in2, HIGH); //Right forward HIGH 
+  digitalWrite(in3, LOW); //Left forward LOW
+  digitalWrite(in4, HIGH); //Left backward HIGH
+}
+
+void stop(){
+  //everything on low
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW); 
+}
 
 
 
